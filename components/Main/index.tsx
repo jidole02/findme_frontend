1b8f07d01f81d68db7ee5baeb9100e8ccdc17cd9
jiwon <jidole041214@naver.com>
2022-03-04 19:29:43 +0900

    update my maker image

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -18,31 +18,30 @@ const Main = () => {
     });
   }
 
-  function drawMyLocation() {
-    window.kakao.maps.load(() => {
-      var markerPosition = new window.kakao.maps.LatLng(x, y);
-
-      // 마커를 생성합니다
-      var marker = new window.kakao.maps.Marker({
-        position: markerPosition,
-      });
-    });
-  }
-
   function draw() {
     window.kakao.maps.load(() => {
       const mapContainer = document.getElementById("map");
       const mapOption = {
-        center: new window.kakao.maps.LatLng(x, y), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
+        center: new window.kakao.maps.LatLng(x, y),
+        level: 3,
       };
-
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-      let marker = new window.kakao.maps.Marker({
+      const imageSrc = "/maker.png", // 마커이미지의 주소입니다
+        imageSize = new window.kakao.maps.Size(120, 120), // 마커이미지의 크기입니다
+        imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+      const markerImage = new window.kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      );
+
+      const marker = new window.kakao.maps.Marker({
         map: map,
         position: new window.kakao.maps.LatLng(x, y),
       });
+      marker.setImage(markerImage);
     });
   }
 
@@ -51,10 +50,8 @@ const Main = () => {
   useEffect(() => {
     if (x !== 0) {
       const mapScript = document.createElement("script");
-
       mapScript.async = true;
       mapScript.src = `http://dapi.kakao.com/v2/maps/sdk.js?appkey=23ff1647b8abe7a607e42f5bbda3e52e&autoload=false`;
-
       document.head.appendChild(mapScript);
 
       const onLoadKakaoMap = () => {
