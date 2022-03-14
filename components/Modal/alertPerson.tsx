9e04c9673e4a96a2c634b49f45f1231786018287
jiwon <jidole041214@naver.com>
2022-03-14 20:22:56 +0900

    add alert person

--- components/Modal/alertPerson.tsx
+++ components/Modal/alertPerson.tsx
@@ -5,14 +5,22 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux";
 import { setModal } from "./../../redux/modal";
+import { ChangeEvent, useState } from "react";
 
 const AlertPerson = () => {
   const person_id = useSelector((state: RootState) => state.PersonReducer.id);
   const dispatch = useDispatch();
+  const [description, setDescription] = useState("");
+
+  function handleInput(event: ChangeEvent<HTMLTextAreaElement>) {
+    setDescription(event.target.value);
+  }
 
   function alertPerson() {
     axios
-      .delete(`${process.env.NEXT_PUBLIC_URL}/write/alert?id=${person_id}`)
+      .delete(
+        `${process.env.NEXT_PUBLIC_URL}/write/alert?id=${person_id}&description=${description}`
+      )
       .then((res) => {
         alert("신고해주셔서 감사합니다");
         dispatch(setModal(null));
@@ -24,7 +32,10 @@ const AlertPerson = () => {
         본 이메일은 경찰로 전달됩니다 <br />
         작성 후 경찰로 신고해주세요
       </h1>
-      <textarea placeholder="찾은 장소, 실종자 상태를 적어주세요" />
+      <textarea
+        placeholder="찾은 장소, 실종자 상태를 적어주세요"
+        onChange={handleInput}
+      />
       <s.ConfirmButton onClick={alertPerson}>작성 완료</s.ConfirmButton>
       <CloseButton />
     </Wrapper>
